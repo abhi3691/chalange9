52489e96394439a86b6b8f90973dc8cf67a52cf4
ABHINAND <abhinandazokdio@gmail.com>
2021-10-18 15:33:41 +0530

    updated

--- src/TabContent.js
+++ src/TabContent.js
@@ -1,11 +1,14 @@
 import React,{ Component } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
-
+import Icon from 'react-native-vector-icons/MaterialIcons'
 export default class TabContent extends Component  {
   render(){ 
   return (
     <View style={styles.container}>
         <View  style={styles.searchContainer} >   
+        <Icon
+        name ='search' size={24}
+        />
       <TextInput
       placeholder ='Search for Service'
       
